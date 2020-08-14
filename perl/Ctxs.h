8c378d345d280ed0f5216d1885013b94569dda24
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-08-15 00:01:13 +0200

    Create Ctxs.h

---
+++ perl/Ctxs.h
@@ -0,0 +1,21 @@
+#ifndef __CTXS_H
+#define __CTXS_H
+
+
+#include <lorcon2/lorcon.h>
+#include <lorcon2/lorcon_packet.h>
+#include <lorcon2/lorcon_multi.h>
+#include <lorcon2/lorcon_ieee80211.h>
+#include <lorcon2/lorcon_forge.h>
+#include <lorcon2/drv_madwifing.h>
+#include <lorcon2/airpinject.h>
+#include <lorcon2/drv_airjack.h>
+#include <lorcon2/drv_file.h>
+#include <lorcon2/drv_mac80211.h>
+#include <lorcon2/drv_tuntap.h>
+#include <lorcon2/iwcontrol.h>
+#include <lorcon2/lorcon_int.h>
+#include <lorcon2/nl80211_control.h>
+#include <lorcon2/lorcon_packasm.h>
+
+#endif /* __CTXS_H */
