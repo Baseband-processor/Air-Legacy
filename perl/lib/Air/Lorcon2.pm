b30a79bb2435bdaba5a44e5982b7dec7a43bef6b
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-09-02 19:50:44 +0200

    added first part of POD description

--- perl/lib/Air/Lorcon2.pm
+++ perl/lib/Air/Lorcon2.pm
@@ -1067,3 +1067,72 @@ __PACKAGE__->bootstrap($VERSION);
 1;
 
 __END__
+
+=head1 NAME
+Air::Lorcon2  A fast, portable and efficient library based on Lorcon2. Written in XS for perl penetration tester and wireless-security experts
+
+=head1 SYNOPSIS
+
+use strict;
+use Net::Pcap qw( pcap_lookupdev );
+use Data::Dumper qw(Dumper);
+use Air::Lorcon2 qw(:lorcon); # This will export every lorcon2 subroutines
+
+my $pcap_err = '';
+my $pcap_interface = pcap_lookupdev( \$pcap_err ); # This will give us the best interface avaiable for sniffing 
+
+print Dumper( Air::Lorcon2::lorcon_list_drivers() ) or die $!;
+my $driver = <STDIN>;
+chomp( $driver ); # Delete the 'ret' character from the $driver string
+my $drv = Air::Lorcon2::lorcon_find_driver( $driver );
+
+my $context = Air::Lorcon2::lorcon_create($pcap_interface, $drv) or die $!;
+
+# From here we have access to an huge number of functions, some simple examples are:
+
+Air::Lorcon2::lorcon_ifdown( $context ) or die Air::Lorcon2::lorcon_get_error( $context ); # Set interface 'down'
+Air::Lorcon2::lorcon_ifup( $context ) or die Air::Lorcon2::lorcon_get_error( $context ); # Set interface 'up'
+
+my $channel = 2;
+
+Air::Lorcon2::lorcon_set_channel( $context, $channel ) or die Air::Lorcon2::lorcon_get_error( $context ); # set channel to 2
+Air::Lorcon2::lorcon_get_channel( $context ) or die Air::Lorcon2::lorcon_get_error( $context ); # return the channel, in this case 2
+
+Air::Lorcon2::lorcon_open_inject (  $context ) or die Air::Lorcon2::lorcon_get_error( $context ); # set the injection mode
+Air::Lorcon2::lorcon_open_monitor(  $context ) or die Air::Lorcon2::lorcon_get_error( $context ); # set the monitor mode
+Air::Lorcon2::lorcon_open_injmon (  $context ) or die Air::Lorcon2::lorcon_get_error( $context ); # set both
+
+# We can also initialize our preferred network driver using
+
+Air::Lorcon2::drv_madwifing_init( $context ); 
+
+# ||
+
+Air::Lorcon2::drv_mac80211_init( $context ); 
+
+# And if we add a packet the possible uses  grows exponentially:
+
+my $Packet = "\xdd\x09\x00\x50\xf2\x04\x10\x4a\x00\x01\x10"; # WPS probe packet taken by Air::Reaver, another my module for Reaver
+
+# || 
+
+my $Packet = Air::Lorcon2::Packet_to_hex("sample_packet"); # return a hexadecimal version of "sample_packet" with \x format
+
+Air::Lorcon2::lorcon_send_bytes( $context, length($Packet), \$Packet ); # this will send the raw bytes though the network
+
+# NOTE:
+# Since version 17.6 is possible to use also this simplified function:
+
+print Air::Lorcon2::Send_Bytes( $context, $Packet); 
+# The $Packet length is processed in the Back-End.
+
+=head1 DESCRIPTION
+
+
+=head1 AUTHOR
+ 
+<lt>Edoardo Mantovani<gt> 
+  
+=head1 COPYRIGHT AND LICENSE
+Copyright (C) 2020 by Edoardo Mantovani, aka BASEBAND
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself, either Perl version 5.8.8 or, at your option, any later version of Perl 5 you may have available.
