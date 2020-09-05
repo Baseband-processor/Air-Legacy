39ac65d7c2f5d95ff883d0f5b521377827820461
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-09-05 16:39:48 +0200

    first version of Air::Lorcon2::nl80211

---
+++ perl/lib/Air/Lorcon/nl80211.pm
@@ -0,0 +1,10 @@
+package Air::Lorcon2::nl80211;
+
+# Made by Edoardo Mantovani, 2020
+# Note: this extensions is one of the first drop for using the nl80211-linux driver, the header file are based on the libnl3 installed
+# automatically with the Makefile
+
+use strict;
+use warnings;
+
+
