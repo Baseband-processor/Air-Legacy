231edb56577f105acc17d85df5a1301cd8dbbc17
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-09-10 08:39:48 +0200

    deleted useless code

--- perl/examples/SendBytes.pl
+++ perl/examples/SendBytes.pl
@@ -3,20 +3,20 @@
 use strict;
 use Air::Lorcon2 qw( :lorcon );
 use Data::Dumper qw( Dumper );
-print Dumper( Air::Lorcon2::lorcon_list_drivers() );
+print Dumper(  lorcon_list_drivers() );
 print "insert your driver name: ";
 my $driver = <STDIN>;
 chomp $driver;
 
 
 my $drv = lorcon_find_driver( $driver ) or die $!;
-my $lorcon = Air::Lorcon2::lorcon_create("wlo1", $drv ) or die $!;
+my $lorcon =  lorcon_create("wlo1", $drv ) or die $!;
 
 my $packet = "\x00\x00\x00\x00\x00\x00";
 
-if( Air::Lorcon2::lorcon_open_injmon( $lorcon ) == -1 || Air::Lorcon2::lorcon_open_injmon( $lorcon ) == -255 ){	
+if(  lorcon_open_injmon( $lorcon ) == -1 ||  lorcon_open_injmon( $lorcon ) == -255 ){	
      die $!; # open monitor mode and injection mode failed
 }
-print Air::Lorcon2::lorcon_send_bytes($lorcon, length($packet), \$packet);
+print  lorcon_send_bytes($lorcon, length($packet), \$packet);
 
 
