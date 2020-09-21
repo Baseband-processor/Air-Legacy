5c21c06db8ddd32597b7b28b32b592b37ced6966
Edoardo Mantovani <Baseband@cpan.org>
2020-09-21 18:46:30 +0200

    Update Readme-Example.pl

--- perl/examples/Readme-Example.pl
+++ perl/examples/Readme-Example.pl
@@ -1,14 +1,16 @@
-#!/usr/bin/perl
-
 use strict;
 use Net::Pcap qw( pcap_lookupdev );
 use Data::Dumper qw(Dumper);
-use Air::Lorcon2 qw(:lorcon); # This will export every lorcon2 subroutines
+use Air::Lorcon2 qw(:lorcon); # This will export every lorcon2's subroutines
 
 my $pcap_err = '';
 my $pcap_interface = pcap_lookupdev( \$pcap_err ); # This will give us the best interface avaiable for sniffing 
 
 print Dumper( lorcon_list_drivers() ) or die $!;
+
+# NOTE: lorcon_list_drivers will show supported drivers avaiable in the current host, while tx80211_getcardlist function
+# will show the lorcon's supported network cards list
+
 my $driver = <STDIN>;
 chomp( $driver ); # Delete the 'ret' character from the $driver string
 my $drv = lorcon_find_driver( $driver );
@@ -47,8 +49,43 @@ my $Packet = Packet_to_hex("sample_packet"); # return a hexadecimal version of "
 
 lorcon_send_bytes( $context, length($Packet), \$Packet ); # this will send the raw bytes though the network
 
+$Packet = undef;
+
 # NOTE:
 # Since version 17.6 is possible to use also this simplified function:
 
 print Send_Bytes( $context, $Packet); 
 # The $Packet length is processed in the Back-End.
+
+
+my $lcpa = lcpa_init();
+$Packet = packet_from_lcpa( $context, $lcpa ); # return a AirLorconPacket variable
+
+# decode the packet
+lorcon_packet_decode( $Packet );
+
+# Get a valid Pcap object, usefull for built-in (or with Net::Pcap) routines
+
+my $pcap = lorcon_get_pcap( $context );
+
+# Set frequency using libpcap
+
+pcap_can_set_rfmon( $pcap );
+
+# Send packet using libpcap
+
+pcap_sendpacket( $pcap, $Packet, length( $Packet ) );
+
+# Note: pcap_sendpacket and pcap_inject are almost the same function, the only difference stands in the output: for pcap_inject it will be the packet's number of bytes
+
+# For more info see: https://linux.die.net/man/3/pcap_inject
+
+# Lorcon2 offers also the possibility of sending bytes using specific drivers, some example are as follow:
+
+madwifing_sendpacket( $context, $Packet );
+mac80211_sendpacket( $context, $Packet );
+
+# Note that $Packet has _lorcon_packet_t_ type
+
+my $raw_bytes = "\x00\x00\x00\x00\x00";
+tuntap_sendbytes( $context, length( $raw_bytes ), \$raw_bytes );
