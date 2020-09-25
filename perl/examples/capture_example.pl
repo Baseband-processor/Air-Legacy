1db98df32a3b145957eb5435f711f4e93c872be0
Edoardo Mantovani <Baseband@cpan.org>
2020-09-25 20:05:56 +0200

    added perl version of "capture_example.c"

---
+++ perl/examples/capture_example.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+
+# Made by Edoardo Mantovani, 2020
+
+# perl version of capture_example.c
+
+use strict;
+use warnings;
+
+use Air::Lorcon2 qw( :lorcon );
+
+# defining our packet disassembly sub
+
+sub apitest_packet_hdlr{
+  my ( $context, $packet, $user ) = @_;
+  my $dot;
+  printf("apitest - %s drv %s got packet len %d\n", lorcon_get_capiface( $context ), lorcon_get_driver_name( $context ), length( $packet ) );
+    
+  my $len = lorcon_packet_to_dot3( $packet, \$dot );
+  
+  if( ! $len || undef( $len ) ){
+    die "error with $len var!\n";
+    }
+    
+	printf("dot3 length %d\n", $len);
+
+	lorcon_packet_free($packet);
+
+}
+
+BEGIN{
+  use Data::Dumper qw( Dumper );
+  sleep(1);
+  # print supported drivers list
+  print Dumper( lorcon_list_drivers() );
+  my $choose = <STDIN>;
+  chop( $choose );
+  my $drv = lorcon_find_driver( $choose );
+  if( ! $drv ){
+    die("driver error!\n");
+  }
+  # detect wireless interface name
+  
+  use Net::Pcap qw( pcap_lookupdev );
+  my $pcap_err = '';
+  my $pcap_dev = pcap_lookupdev( \$pcap_err );
+  # create lorcon
+  my $lorcon = lorcon_create(  $pcap_dev, $drv );
+  
+  # open inject and monitor mode
+  lorcon_open_injmon( $lorcon );
+
+  # free the driver list
+  lorcon_free_driver_list( $drv );
+  
+  # start looping a packet
+ 	
+  lorcon_loop( $lorcon , 0, &apitest_packet_hdlr, undef);
+
+  lorcon_free( $lorcon );
+
+
+}
+
+
+# END
