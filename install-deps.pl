e8cbc02d5c9308183b0f73ee5853362f2615fe28
Edoardo Mantovani <Baseband@cpan.org>
2020-09-29 15:32:37 +0200

    deleted END block, modified dependencies: now XS::Install will be automatically force-installed in every linux distribution

--- install-deps.pl
+++ install-deps.pl
@@ -18,22 +18,23 @@ sleep(1);
 if( distribution_name() =~ /debian/ || distribution_name() =~ /ubuntu/){  # for debian/ubuntu Oses
   	system("sudo apt update ");
 	system("sudo apt install flex bison libpcap* dh-autoreconf");
+	system("cpan -fi XS::Install");
   }    
   
   elsif( distribution_name() =~ "fedora" || distribution_name() =~ "centos" ||  distribution_name() =~ "rhel" ){ # for Fedora/CentOS/RHEL
     system("sudo yum install flex bison libpcap* dh-autoreconf");
+    system("cpan -fi XS::Install");
   }elsif( distribution_name() =~ "openSUSE" ){
     system("sudo zypper install flex bison libpcap* dh-autoreconf");
+    system("cpan -fi XS::Install");
   }elsif( distribution_name() =~ "Mageia" ){
     system("sudo urpmi flex bison libpcap* dh-autoreconf");
+    system("cpan -fi XS::Install");
   }elsif( distribution_name() =~ "Alpine"){
     system("sudo apk add flex bison libpcap* dh-autoreconf");  
+    system("cpan -fi XS::Install");
   
   }else{
     print "every dependencies accomplished!\n";
   
 }		}
-sub  END{
-  # INSTALL PERL MODULE REQUIREMENTS  
-  # WIP
-  }
