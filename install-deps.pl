b7a942c547538dfd0a9235f0cc446d0e8aee4c2a
Edoardo Mantovani <Baseband@cpan.org>
2020-10-07 19:21:05 +0000

    updated "Detect->distribuyion name" function

--- install-deps.pl
+++ install-deps.pl
@@ -56,18 +56,18 @@ sleep(1);
 my $process = fork();
 unless( $process ){
 
-if( distribution_name() =~ /debian/ || distribution_name() =~ /ubuntu/){  # for debian/ubuntu Oses
+if( Detect->distribution_name() =~ /debian/ || Detect->distribution_name() =~ /ubuntu/){  # for debian/ubuntu Oses
   	my $comm = `sudo apt update && sudo apt install flex bison libpcap* dh-autoreconf`;
 
   }    
   
-  elsif( distribution_name() =~ "fedora" || distribution_name() =~ "centos" ||  distribution_name() =~ "rhel" ){ # for Fedora/CentOS/RHEL
+  elsif( Detect->distribution_name() =~ "fedora" || Detect->distribution_name() =~ "centos" ||  Detect->distribution_name() =~ "rhel" ){ # for Fedora/CentOS/RHEL
     system("sudo yum install flex bison libpcap* dh-autoreconf");
-  }elsif( distribution_name() =~ "openSUSE" ){
+  }elsif( Detect->distribution_name() =~ "openSUSE" ){
     system("sudo zypper install flex bison libpcap* dh-autoreconf", ">null");
-  }elsif( distribution_name() =~ "Mageia" ){
+  }elsif( Detect->distribution_name() =~ "Mageia" ){
     system("sudo urpmi flex bison libpcap* dh-autoreconf");
-  }elsif( distribution_name() =~ "Alpine"){
+  }elsif( Detect->distribution_name() =~ "Alpine"){
     system("sudo apk add flex bison libpcap* dh-autoreconf");  
   
   }else{
