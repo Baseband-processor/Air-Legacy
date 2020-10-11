05fc18f535ffb0942067c3d95d7019106949578b
Edoardo Mantovani <Baseband@cpan.org>
2020-10-11 16:13:54 +0200

    added threads support for installing perl libraries

--- install-deps.pl
+++ install-deps.pl
@@ -6,6 +6,7 @@
 # version 1.35: added APT interface
 # PRE-version 1.25: added better (and more intuible) front-end graphic
 
+use threads;
 use Term::ANSIColor;
 
 BEGIN{
@@ -79,7 +80,7 @@ print "Every requirement has been installed!\n";
 
 sub install_libs{	
 
-		if(system("sudo cpan -fi  @_ ") ){
+		if(threads->create( sub{ system("sudo cpan -fi  @_ "); } ) ){
 			print colored(['bright_red on_black'],"Succesfully installed @_", "\r");	
 		}
 }
