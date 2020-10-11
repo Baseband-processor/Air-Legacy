26409852b3a31a4e815b4f95a90da5ab2303b74d
Edoardo Mantovani <Baseband@cpan.org>
2020-10-11 09:51:55 +0200

    first version of Air::Lorcon2::Extra pm file, still under development
    stay tuned!

--- perl/lib/Air/Lorcon2/Extra.pm
+++ perl/lib/Air/Lorcon2/Extra.pm
@@ -4,3 +4,29 @@
 # Air::Lorcon2's HASHING support
 
 package Air::Lorcon2::Extra;
+
+our $VERSION = '27.75';
+
+use strict;
+no strict subs';
+no strict 'refs';
+use warnings;
+use base qw(Exporter DynaLoader);
+
+our %EXPORT_TAGS = (
+  sha1 => [qw(
+    sha1_process
+    sha1_update
+    sha1_finish
+    sha1_starts
+    sha1_hmac_starts
+    sha1_hmac_update
+    sha1_hmac_finish
+    sha1_hmac  
+  )],
+  
+);
+@{ $EXPORT_TAGS{ sha1 } },
+  
+  
+1;
