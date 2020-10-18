bf25fbad00e06625b64803d0cf6ac435182db5a8
Edoardo Mantovani <Baseband@cpan.org>
2020-10-18 15:57:22 +0200

    added __NET_CFG80211_H condition

--- C/cfg80211_control.h
+++ C/cfg80211_control.h
@@ -1,6 +1,7 @@
 // Made by Edoardo Mantovani, 2020
 
 // control cfg80211 eco-system
+#ifdef __NET_CFG80211_H
 
 #include "config.h"
 
@@ -13,4 +14,5 @@ int cfg80211_init(lorcon_t *);
 int tx80211_cfg80211_capabilities();
 lorcon_driver_t *drv_cfg80211_listdriver(lorcon_driver_t *);
 
+#endif
 #endif
