36ba383438d335b6e948a9fce9b4c740f3c8a1cc
Edoardo Mantovani <Baseband@cpan.org>
2020-10-19 16:21:59 +0200

    added support functions and relative headers

--- C/cfg80211_control.c
+++ C/cfg80211_control.c
@@ -6,6 +6,9 @@
 #include <stdint.h>
 #include <linux/types.h>
 #include <asm/byteorder.h>
+#include <linux/netdevice.h>
+#include <linux/sched.h>
+#include <linux/mutex.h>
 
 // NOTE: types.h contains __le32 and other types, suggested static linking
 
@@ -61,6 +64,120 @@ void convert_key_from_CPU(struct brcmf_wsec_key *key, struct brcmf_wsec_key_le *
 	key_length->iv_initialized = cpu_to_le32(key->iv_initialized);
 	memcpy(key_length->data, key->data, sizeof(key->data));
 	memcpy(key_length->ea, key->ea, sizeof(key->ea));
+}
+
+
+typedef struct { int counter; } atomic_t;
+#define atomic_read(v)		((v)->counter)
+
+int brcmf_get_pend_8021x_cnt(struct brcmf_pub *drvr){
+	return atomic_read(&drvr->pend_8021x_cnt);
+}
+
+#define MAX_WAIT_FOR_8021X_TX	10
+#define TASK_INTERRUPTIBLE 1
+#define TASK_RUNNING 0
+
+int brcmf_netdev_wait_pend8021x(struct net_device *ndev)
+{
+	struct brcmf_if *ifp = netdev_priv(ndev);
+	struct brcmf_pub *drvr = ifp->drvr;
+	int timeout = 10 * HZ / 1000;
+	int ntimes = MAX_WAIT_FOR_8021X_TX;
+	int pend = brcmf_get_pend_8021x_cnt(drvr);
+
+	while (ntimes && pend) {
+		if (pend) {
+			set_current_state(TASK_INTERRUPTIBLE); // set_current_state() changes the state of the currently executing process from TASK_RUNNING to TASK_INTERRUPTIBLE.
+			schedule_timeout(timeout);
+			set_current_state(TASK_RUNNING);
+			ntimes--;
+		}
+		pend = brcmf_get_pend_8021x_cnt(drvr);
+	}
+	return pend;
+}
+
+u32
+brcmf_create_bsscfg(s32 bssidx, char *name, char *data, u32 datalen, char *buf, u32 buflen) {
+	const s8 *prefix = "bsscfg:";
+	s8 *p;
+	u32 prefixlen;
+	u32 namelen;
+	u32 iolen;
+	__le32 bssidx_le;
+
+	if (bssidx == 0){
+		return brcmf_create_iovar(name, data, datalen, buf, buflen);
+	}
+	prefixlen = strlen(prefix);
+	namelen = strlen(name) + 1; /* lengh of iovar  name + null */
+	iolen = prefixlen + namelen + sizeof(bssidx_le) + datalen;
+
+	if (buflen < iolen) {
+		brcmf_err("buffer is too short\n");
+		return 0;
+	}
+
+	p = buf;
+	memcpy(p, prefix, prefixlen);
+	p += prefixlen;
+	memcpy(p, name, namelen);
+	p += namelen;
+	bssidx_le = cpu_to_le32(bssidx);
+	memcpy(p, &bssidx_le, sizeof(bssidx_le));
+	p += sizeof(bssidx_le);
+	if (datalen){
+		memcpy(p, data, datalen);
+	}
+	return iolen;
+}
+
+s32
+brcmf_fil_cmd_data(struct brcmf_if *ifp, u32 cmd, void *data, u32 len, bool set){
+	struct brcmf_pub *drvr = ifp->drvr;
+	s32 err;
+
+	if (drvr->bus_if->state != BRCMF_BUS_UP) {
+		brcmf_err("bus is down. we have nothing to do.\n");
+		return -EIO;
+	}
+
+	if (data != NULL)
+		len = min_t(uint, len, BRCMF_DCMD_MAXLEN);
+	if (set){
+		err = brcmf_proto_set_dcmd(drvr, ifp->ifidx, cmd, data, len);
+	}else{
+		err = brcmf_proto_query_dcmd(drvr, ifp->ifidx, cmd, data, len);
+	}
+	if (err >= 0){
+		return 0;
+	}
+	brcmf_dbg(FIL, "Failed: %s (%d)\n", brcmf_fil_get_errstr((u32)(-err)), err);
+	return -EBADE;
+}
+
+s32 brcmf_fil_bsscfg_data_set(struct brcmf_if *ifp, char *name, void *data, u32 len)
+{
+	struct brcmf_pub *drvr = ifp->drvr;
+	s32 err;
+	u32 buflen;
+
+	mutex_lock(&drvr->proto_block);
+
+	brcmf_dbg(FIL, "bssidx=%d, name=%s, len=%d\n", ifp->bssidx, name, len);
+	brcmf_dbg_hex_dump(BRCMF_FIL_ON(), data, min_t(uint, len, MAX_HEX_DUMP_LEN), "data");
+
+	buflen = brcmf_create_bsscfg(ifp->bssidx, name, data, len, drvr->proto_buf, sizeof(drvr->proto_buf));
+	if (buflen) {
+		err = brcmf_fil_cmd_data(ifp, BRCMF_C_SET_VAR, drvr->proto_buf, buflen, true);
+	} else {
+		err = -EPERM;
+		brcmf_err("Creating bsscfg failed\n");
+	}
+
+	mutex_unlock(&drvr->proto_block);
+	return err;
 }
 
 int send_key_to_dongle(struct brcmf_if *ifp, struct brcmf_wsec_key *key){
