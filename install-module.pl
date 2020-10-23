295a4dbc1cef942796e226cc1b1a5e439415b460
Edoardo Mantovani <Baseband@cpan.org>
2020-10-23 15:53:07 +0200

    updated installer, added forks

--- install-module.pl
+++ install-module.pl
@@ -1,8 +1,15 @@
 #!/usr/bin/perl
 # Made by Edoardo Mantovani, 2020
 
+use strict;
+use warnings;
 use App::Cpan;
+require "./include/Processes.pm";
 
-App::Cpan->run( @ARGV );
+my $process = Proc::Simple->new();
 
+$process->start( sub {
+  App::Cpan->run( @ARGV );
+  });
+  
 #END
