59ba8747e24061946892e848a82ba4f83ce9c621
Edoardo Mantovani <Baseband@cpan.org>
2020-10-31 21:08:23 +0100

    created C file for reaver structure returns functions

--- C/reaver.c
+++ C/reaver.c
@@ -3,40 +3,65 @@
 
 // export all struct
 
-typedef struct association_request_management_frame{
+struct association_request_management_frame{
 	le16 capability;
 	le16 listen_interval;
-}ASSOCIATION_REQUEST_MANAGEMENT_FRAME;
+};
 
 
 struct association_response_management_frame{
 	le16 capability;
 	le16 status;
 	le16 id;
-}ASSOCIATION_RESP_MANAGEMENT_FRAME;
+};
 
 struct  beacon_management_frame{
 	unsigned char timestamp[TIMESTAMP_LEN];
 	le16 beacon_interval;
 	le16 capability;
-}BEACON_MANAGEMENT_FRAME;
+};
 
-
-struct {
-	int proto;
-	int pairwise_cipher;
-	int group_cipher;
-	int key_mgmt;
-	int capabilities;
-	size_t num_pmkid;
-	const u8 *pmkid;
-	int mgmt_group_cipher;
-}wpa_ie_data;
-
-typedef struct wpa_ie_data        WPA_IE_DATA;
 
 struct authentication_management_frame{
 	le16 algorithm;
 	le16 sequence;
 	le16 status;
-}AUTH_MANAGEMENT_FRAME;
+};
+
+// functions
+
+struct association_request_management_frame *assoc_request_meta()
+{
+	struct association_request_management_frame *c =  (struct association_request_management_frame *) malloc(sizeof(struct association_request_management_frame));
+	c->capability = NULL;
+	c->listen_interval = NULL;
+	return c;	
+}
+
+
+struct association_response_management_frame *assoc_response_meta()
+{
+	struct association_response_management_frame *c =  (struct association_response_management_frame *) malloc(sizeof(struct association_response_management_frame));
+	c->capability = NULL;
+	c->status = NULL;
+	c->id = NULL;
+	return c;	
+}
+
+
+struct beacon_management_frame *beacon_management_meta()
+{
+	struct beacon_management_frame *c =  (struct beacon_management_frame *) malloc(sizeof(struct beacon_management_frame));
+	c->capability = NULL;
+	c->beacon_interval = NULL;
+	return c;	
+}
+
+struct authentication_management_frame *auth_management_meta()
+{
+	struct authentication_management_frame *c =  (struct authentication_management_frame *) malloc(sizeof(struct authentication_management_frame));
+	c->sequence = NULL;
+	c->status = NULL;
+	c->algorithm = NULL;
+	return c;	
+}
