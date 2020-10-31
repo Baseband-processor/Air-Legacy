dbcfb72f4fec937ee0ea69ca5e079a17b57b4b87
Edoardo Mantovani <Baseband@cpan.org>
2020-10-31 21:12:04 +0100

    added reaver header

---
+++ C/reaver.h
@@ -0,0 +1,22 @@
+#ifndef __REAVER_H
+#define __REAVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <string.h>
+
+#ifndef LE16_DEFINED
+#define LE16_DEFINED
+typedef uint16_t le16;
+typedef uint32_t le32;
+#endif
+
+struct association_request_management_frame *assoc_request_meta();
+struct association_response_management_frame *assoc_response_meta();
+struct beacon_management_frame *beacon_management_meta();
+struct authentication_management_frame *auth_management_meta();
+
+
+#endif
