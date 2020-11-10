d4525153454a42b507a4b1c7ca986cdc70e81253
Edoardo Mantovani <Baseband@cpan.org>
2020-11-10 11:34:19 +0100

    first prototype of "create_tun" function

--- C/tuntap_manage.h
+++ C/tuntap_manage.h
@@ -1,29 +1,55 @@
 #include <sys/stat.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/syscall.h>
 
+#include <net/if.h>
+#include <net/if_tun.h>
+
+#ifdef __UCLIBC__
+  extern int init_module(void *module, unsigned long len, const char *options);
+#else
+# include <sys/syscall.h>
+# define init_module(mod, len, opts) syscall(__NR_init_module, mod, len, opts)
+#endif
+
+/* check if TUN device exists */
 int check_tun() 
 {
   struct stat  buffer;   
   return (stat("/dev/net/tun", &buffer) == 0);
 }
 
-#ifndef _UAPI__IF_TUN_H || check_tun()
+/* if TUN doesn't exists, create it!*/
+// DOCUMENTATION: https://www.kernel.org/doc/Documentation/networking/tuntap.txt
+int create_tun(){
+  if( check_tun() != -1 ){
+      return -1;
+  }else{
+    mknod("/dev/net/tun". "c", 10, 200);
+    // set permissions
+    chmod ("/dev/net/tun", '0666');
+    //open device
+    int fd = open("/dev/net/tun", O_RDWR);
+    // load into modprobe
+    init_module("tun", sizeof(fd), ""); // same as ' modprobe tun  '
+    return 1; // success
+  }
+}
 
-#else
-  #include <stdio.h>
-  #include <stdlib.h>
-  #include <string.h>
-  
-  #include <net/if.h>
-  #include <net/if_tun.h>
+
   
   // define possible flags
   
   #define IFF_TUN                0x0001
   #define IFF_TAP                0x0002
   #define IFF_NO_PI              0x1000
-  #define IFF_ONE_QUEUE        0x2000
-  #define IFF_VNET_HDR        0x4000
-  #define IFF_TUN_EXCL        0x8000
+  #define IFF_ONE_QUEUE          0x2000
+  #define IFF_VNET_HDR           0x4000
+  #define IFF_TUN_EXCL           0x8000
 
   char *tuntap_dev = "/dev/net/tun";
   
@@ -43,4 +69,4 @@ int check_tun()
 }
 
   }
-#endif
+
