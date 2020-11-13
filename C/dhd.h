9492aff1139f98b9d3210cfb55235e968aa53c74
Edoardo Mantovani <Baseband@cpan.org>
2020-11-13 19:51:45 +0100

    fixed comment error

--- C/dhd.h
+++ C/dhd.h
@@ -1,3 +1,4 @@
+/*
 * Copyright (c) 2010 Broadcom Corporation            
 *                 
 * Permission to use, copy, modify, and/or distribute this software for any      
@@ -11,8 +12,9 @@
 * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION    
 * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN     
 * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.        
-*/                 
-                 
+*
+*/
+
 /****************                 
 * Common types *              
 */                 
