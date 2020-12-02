0297b80d772e6d697db9d6ceaa3b0ede8e47ecbc
Edoardo Mantovani <Baseband@cpan.org>
2020-12-02 15:31:04 +0100

    created main airpcap inclusion file

---
+++ perl/c/airpcap.h
@@ -0,0 +1,11 @@
+#ifndef __AIRPCAP_H
+#define __AIRPCAP_H
+
+// Made by Edoardo Mantovani, 2020
+// Main airpcap interface
+
+// code acquired from: https://github.com/hbock/libairpcap-nl
+
+#include "airpcap/airpcap.h"
+
+#endif
