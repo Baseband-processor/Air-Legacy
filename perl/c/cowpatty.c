8a5878bafe3f6277168dddb405bf276584c73ac5
Edoardo Mantovani <Baseband@cpan.org>
2020-12-26 10:23:36 +0100

    added typemaps definitions

--- perl/c/cowpatty.c
+++ perl/c/cowpatty.c
@@ -3,13 +3,13 @@
 
 #include <sys/types.h>
 
-struct hashdb_head
+typedef struct hashdb_head
 {
 	uint32_t magic;
 	uint8_t reserved1[3];
 	uint8_t ssidlen;
 	uint8_t ssid[32];
-};
+}HASHDB;
 
 struct hashdb_rec
 {
@@ -18,12 +18,14 @@ struct hashdb_rec
 	uint8_t pmk[32];
 } __attribute__((packed));
 
+typedef struct hashdb_rec HASHDB_REC;
+
 struct cowpatty_file
 {
 	char ssid[33];
 	FILE * fp;
 	char error[256 - sizeof(FILE *) - 33];
-};
+}COWPA_FILE;
 
 hashdb_head *spawn_hashdb(){
   hashdb_head *COW_database =  ( hashdb_head *)malloc( hashdb_head *);
